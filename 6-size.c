84a4736febd7a8ce95f16e89f7ad8c1695afe0d4
diego <vandeldiegoc@gmail.com>
2020-02-05 21:52:11 -0500

    6. Size is not grandeur, and territory does not make a nation

--- 6-size.c
+++ 6-size.c
@@ -6,9 +6,10 @@
  */
 int main(void)
 {
-printf("Size of int: %ld bytes\n", sizeof(intType));
-printf("Size of float: %ld bytes\n", sizeof(floatType));
-printf("Size of double: %ld bytes\n", sizeof(doubleType));
-printf("Size of char: %ld byte\n", sizeof(charType));
+printf("Size of char: %lu byte\n", sizeof(char));
+printf("Size of int: %lu bytes\n", sizeof(int));
+printf("Size of float: %lu bytes\n", sizeof(lon int));
+printf("Size of double: %lu bytes\n", sizeof(lon long));
+printf("Size of double: %lu bytes\n", sizeof(float));
 return (0);
 }
