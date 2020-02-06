619ff3c38ea7b994aca2b06bcb5c0f948490ffae
diego <vandeldiegoc@gmail.com>
2020-02-05 20:35:25 -0500

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,4 @@
 #include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
 char string[] = "\"Programming is like building a multilingual puzzle";
