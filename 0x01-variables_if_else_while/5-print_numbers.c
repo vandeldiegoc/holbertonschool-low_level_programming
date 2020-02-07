a9c555ed374a4f67ebd5e5af32cee8964ca77292
diego <vandeldiegoc@gmail.com>
2020-02-06 21:54:13 -0500

    6. Numberz

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
