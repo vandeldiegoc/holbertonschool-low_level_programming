dee2802377c556083c9f01952e8769f5f0aa3f96
vandeldiegoc <vandeldiegoc@gmail.com>
2020-02-07 16:54:27 +0000

    10. 00...99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+int c;
+int h;
+c = 0;
+while (c <= 9)
+{
+h = 0;
+while (h <= 9)
+{
+putchar('0' + c);
+putchar('0' + h);
+if (c != 9 || h != 9)
+{
+putchar(',');
+putchar(' ');
+}
+h++;
+}
+c++;
+}
+putchar('\n');
+return (0);
+}
