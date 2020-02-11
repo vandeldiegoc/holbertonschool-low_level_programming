e9d485c306df46105d786cd020785cf421503b45
diegoc <vandeldiegoc>
2020-02-11 09:20:37 -0500

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
-
-void  print_alphabet(void)
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
 {
 char c;
 c = 'a';
-
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
-_putchar('\n'); 
+_putchar('\n');
 }
