a9663ca669534b5a4dd56372a3ceb5a38e389f16
diego <vandeldiegoc@gmail.com>
2020-02-11 23:40:00 -0500

    dsad

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,6 @@
+#include "holberton.h"
+int main(void)
+{
+    times_table();
+    return (0);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * times_table - function that prints the 9 times table
+ * @void: no argument
+ */
+void times_table(void)
+{
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (b == 0)
+{
+_putchar(c + '0');
+}
+else if (c <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(c + '0');
+}
+else if (c >= 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(c / 10 + '0');
+_putchar(c % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
