aa80217ad810e59b70c5bc4476cde7bcd0087897
diego <vandeldiegoc@gmail.com>
2020-02-11 20:48:27 -0500

    sdffn

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,17 @@
 #include "holberton.h"
 /**
-* int _abs(int) -  is a funcion
+*  _abs -  is a funcion
 * @b: is a variable
-* Return: 1 and  + if n is greater than zero, 0 and prints 0 if n is zero, *
+* Return: b
 */
 int _abs(int b)
 {
 if (b < 0)
 {
-return(b * -1);
-}	
+return (b * -1);
+}
 else
 {
-return(b);
+return (b);
 }
 }
