ea47a8dae837b4b2e613e08985eb3c6c6e82f4bb
diegoc <vandeldiegoc>
2020-02-13 15:41:21 -0500

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_line -  is a funcion
+ *@n: is a variable
+ */
+void print_line(int n)
+{
+int y;
+for (y = 0; y < n; y++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
