75fb06d836e77d5752c6409c59c1225c210b10f5
diegoc <vandeldiegoc>
2020-02-13 11:58:28 -0500

    3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul -  is a funcion
+ *@a: is a variable
+ *@b: is a variable
+ *Return: 1 if c is lowercase,  0 otherwise
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_numbers -  is a funcion
+ *
+ */
+void print_numbers(void)
+{
+int c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
