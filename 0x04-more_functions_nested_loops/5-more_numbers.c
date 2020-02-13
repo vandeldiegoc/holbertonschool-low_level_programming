8a3be2a01a3d662ac9d6c23302c4352131014db4
diegoc <vandeldiegoc>
2020-02-13 14:41:01 -0500

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *more_numbers -  is a funcion
+ *
+ */
+void more_numbers(void)
+{
+int c;
+int y;
+for (y = 0; y <= 9; y++)
+{
+for (c = 0; c <= 14; c++)
+{
+if (c >= 9)
+{
+_putchar ((c / 10) + '0');
+}
+_putchar((c % 10) + '0');
+}
+_putchar('\n');
+}
+}
