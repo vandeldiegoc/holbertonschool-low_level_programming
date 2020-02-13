e2fbe02989d89bfd8160936655a0c9d9883e05b8
diegoc <vandeldiegoc>
2020-02-13 17:30:46 -0500

    sad

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_square -  is a funcion
+ *@size: is a variable
+ */
+void print_square(int size)
+{
+int y;
+int h;
+int n = size;
+for (h = 0; h < n; h++)
+{
+for (y = 0; y < n; y++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
