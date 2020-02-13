9090bb72f3d302818ba41e57613060795fe1db60
diegoc <vandeldiegoc>
2020-02-13 15:13:29 -0500

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,11 +11,11 @@ for (y = 0; y <= 9; y++)
 {
 for (c = 0; c <= 14; c++)
 {
-if (c >= 9)
+if (c > 9)
 {
-_putchar ((c / 10) + '0');
+_putchar (c / 10 + '0');
 }
-_putchar((c % 10) + '0');
+_putchar(c % 10 + '0');
 }
 _putchar('\n');
 }
