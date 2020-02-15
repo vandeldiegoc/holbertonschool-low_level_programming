ae10c320055bab1748ee7cbcc254b09d78b6d6bd
diego <vandeldiegoc@gmail.com>
2020-02-14 23:10:42 -0500

    100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+long long a = 612852475143;
+int b = 0;
+int c;
+while (a % 2 == 0)
+{
+b = 2;
+a /= 2;
+}
+while (a != 1)
+{
+while (a % c == 0)
+{
+b = c;
+a /= c;
+}
+c += 2;
+}
+printf("%d\n", b);
+
