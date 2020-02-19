f076b56a1c2fed9037d048d2e9247e8aa9448e78
diego <vandeldiegoc@gmail.com>
2020-02-18 22:04:24 -0500

    2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,14 @@
  * print_rev -  is a funcion
  * @s: string
  */
-
 void print_rev(char *s)
 {
 int j;
 for (j = 0; s[j] != 0; j++)
 {
-_putchar(s[j]);
+}
+for (j -= 1; j >= 0; j--)
+{
 }
 _putchar('\n');
 }
