0eaeff8cd8e44d1fdf3dc8919ff7bc46c0c76753
diego <vandeldiegoc@gmail.com>
2020-02-21 22:18:19 -0500

    8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * *rot13 - is a funtion.
+ * @s: is a variable
+ * Return: s
+ */
+char *rot13(char *s)
+{
+int a, b;
+char alfa[] = "abcdefghijklmnopqrstuvwxyz";
+char beta[] = "nopqrstuvwxyzabcdefghijklmi";
+for (a = 0; s[a] != 0; a++)
+{
+for (b = 0; alfa[b] != 0; b++)
+{
+if (s[a] == alfa[b])
+{
+s[a] = beta[b];
+}
+}
+}
+return (s);
+}
+
