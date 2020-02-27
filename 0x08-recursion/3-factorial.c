8b17f34e4f090bbdc73caa3faa884db9db31299d
diego <vandeldiegoc@gmail.com>
2020-02-26 21:16:28 -0500

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,21 +1,21 @@
 #include "holberton.h"
 /**
  * factorial - is a funtion
- * @s: is a variable
+ * @n: is a variable
  * Return: 1
  */
 int factorial(int n)
 {
 if (n < 0)
-{	
+{
 return (-1);
 }
 else if (n == 1)
-{	
+{
 return (1);
 }
 else
 {
-return(n * factorial(n - 1));
+return (n * factorial(n - 1));
 }
 }
