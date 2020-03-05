a9ccf54230be867d34ee73ea19f4ec9cbe7d61d5
diego <diego>
2020-03-05 18:37:42 -0500

    3

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * *_calloc-  is a funcion
+ * @nmemb: is a variable
+ * @size: is a variable
+ * Return: 0
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int a;
+if (nmemb == 0 || size == 0)
+{
+return (NULL);
+}
+char *ptr;
+ptr = malloc(size * nmemb);
+if (ptr == NULL)
+{
+return (NULL);
+}
+for (a = 0; a <= size; a++)
+{
+ptr[a] = 0;
+}
+return (ptr);
+}
