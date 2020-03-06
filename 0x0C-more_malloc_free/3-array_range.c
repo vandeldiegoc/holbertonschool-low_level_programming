b04cfc73085c0f95fd518304fb051e5f9a1a941a
diego <diego>
2020-03-05 19:52:02 -0500

    3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * *array_range -  is a funcion
+ * @min: is a variable
+ * @max: is a variable
+ * Return: 0
+ */
+int *array_range(int min, int max)
+{
+int a, b = 0;
+int *ptr;
+if (min > max)
+{
+return (NULL);
+}
+ptr = malloc(sizeof(*ptr) * (max - min) + 1);
+
+if (ptr == NULL)
+{
+return (NULL);
+}
+
+for (a = min; a <= max; a++, b++)
+{
+ptr[b] = a;
+}
+return (ptr);
+}
+
