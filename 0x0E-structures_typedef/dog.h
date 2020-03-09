89b199d5c1db34857ec23ab07191995c8f2baa55
diego <diego>
2020-03-09 18:57:47 -0500

    0. Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef	DOG_H
+#define DOG_H
+/**
+ * struct dog - Short description
+ * @name:i First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
