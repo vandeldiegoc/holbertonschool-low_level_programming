5292d72764372e0e50108211b036caf7c1cfb910
diego <vandeldiegoc@gmail.com>
2020-03-09 22:11:25 -0500

    dasd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,7 @@ else if ((*d).name == NULL)
 (*d).name = "(nil)";
 else if ((*d).owner == NULL)
 (*d).owner = "(nil)";
-else
-{
 printf("Name: %s\n", (*d).name);
 printf("Age: %f\n", (*d).age);
 printf("Owner: %s\n", (*d).owner);
-}
 }
