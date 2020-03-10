fc7eb078cf7fc42e0b4334759621fda0f0bb91fa
diego <diego>
2020-03-09 19:54:17 -0500

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef	DOG_H
 #define DOG_H
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct dog - Short description
  * @name:i First member
@@ -15,4 +17,5 @@ float age;
 char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
