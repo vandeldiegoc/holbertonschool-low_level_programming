853c74356c0eccfea7be3853d221cdab1d30a7ce
diego <diego>
2020-03-09 20:21:49 -0500

    ewqd

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+/**
+ * init_dog - Short description
+ * @name:i First member
+ * @age: Second member
+ * @owner: Third member
+ * @d: Third member
+ *
+ * Description: Longer description
+ */
+void print_dog(struct dog *d);
+{
+if (d == NULL)
+return (0);
+if ((*d).name == NULL)
+printf ("NIL");
+if ((*d).age == NULL)	
+printf ("NIL");
+if ((*d).owner == NULL)	
+printf ("NIL");
+
+}
