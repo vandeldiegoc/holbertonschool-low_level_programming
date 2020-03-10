54bdcbb9828ceb0160387cdd3e54541f90265e26
diego <diego>
2020-03-09 19:36:06 -0500

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - Short description
+ * @name:i First member
+ * @age: Second member
+ * @owner: Third member
+ * @d: Third member
+ *
+ * Description: Longer description
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+(*d). name = name;
+(*d). age = age;
+(*d). owner = owner;
+}
