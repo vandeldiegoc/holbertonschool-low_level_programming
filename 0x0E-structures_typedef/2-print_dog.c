20bdc88699197f0adcc06320cdc6e406900663a1
diego <vandeldiegoc@gmail.com>
2020-03-09 22:13:42 -0500

    dasd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@ void print_dog(struct dog *d)
 {
 if (d == NULL)
 return;
-else if ((*d).name == NULL)
+if ((*d).name == NULL)
 (*d).name = "(nil)";
-else if ((*d).owner == NULL)
+if ((*d).owner == NULL)
 (*d).owner = "(nil)";
 printf("Name: %s\n", (*d).name);
 printf("Age: %f\n", (*d).age);
