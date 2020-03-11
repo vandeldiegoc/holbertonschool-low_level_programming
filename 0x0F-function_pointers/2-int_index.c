2c0911ef83b5c84a802178176f506253b0c0bff8
diego <diego>
2020-03-11 17:44:09 -0500

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -  is a funcion
+ * @array: is a variable
+ * @size: is a variable
+ * @cmp: is a variable
+ * Return: void
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int a;
+if (size == 0 && array == NULL && cmp == NULL)
+{
+return (-1);
+}
+for (a = 0; a < size; a++)
+{
+if (cmp(array[a]) != 0)
+return (a);
+}
+return (-1);
+}
