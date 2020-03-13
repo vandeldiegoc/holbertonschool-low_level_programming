cece81dd9c7667c539a5c754e22f3231716a88cd
diego <diego>
2020-03-13 17:14:30 -0500

    2. One woman's constant is another woman's variable

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - is a funcion that add the ellipsis (...)
+ * @n: number of number inside ellipsi
+ * @separator: number of number inside ellipsi
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int f;
+char *a;
+va_list v_l;
+va_start(v_l, n);
+for (f = 0; f < n; f++)
+{
+a = va_arg(v_l, char *);
+if (a != NULL)
+{
+printf("%s", a);
+}
+else
+{
+printf("nill");
+}
+if (f != (n - 1))
+{
+printf("%s", separator);
+}
+}
+printf("\n");
+}
+
