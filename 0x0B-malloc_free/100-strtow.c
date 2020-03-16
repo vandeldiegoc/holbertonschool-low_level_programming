41ccb19d1befff8c21bb19f8c9d53d46764442c3
diego <vandeldiegoc@gmail.com>
2020-03-16 11:25:29 -0500

    1

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * strtow - Return a pointer to an array of strings
+ * @str: string to add
+ *
+ * Return: pointer to array of strings
+ */
+char **strtow(char *str)
+{
+	return;
+}
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void main(int argc, char **argv)
+{
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+}
