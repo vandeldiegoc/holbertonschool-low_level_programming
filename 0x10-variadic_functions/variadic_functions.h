caccc60344464370d3d5fef6e64bf2607a9f723b
diego <vandeldiegoc@gmail.com>
2020-03-18 13:15:04 -0500

    dsds

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,24 +3,20 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 /**
  * struct op - Struct op
  *
  * @op: The operator
 	 * @f: The function associated
  */
-
-
 typedef struct op
 {
-  char *op;
-  void (*f) ();
+char *op;
+void (*f)();
 } op_t;
 
 
