3774cb82751fcceedd4f5b815328feecd484beeb
diego <vandeldiegoc@gmail.com>
2020-03-20 22:16:46 -0500

    2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,23 @@
 #include "lists.h"
-
+/**
+ * _strlen - is a function
+ * @str: is a variable
+ *  Return: 0
+ */
 unsigned int _strlen(char *str)
 {
 unsigned int i;
 for (i = 0; str[i]; i++)
-;
+{
+}
 return (i);
 }
-
+/**
+ * add_node - is a function
+ * @head: is a variable
+ * @str: is a variable
+ *  Return: new
+ */
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
@@ -16,14 +26,15 @@ if (str == NULL)
 return (NULL);
 }
 new = malloc(sizeof(list_t));
-if (new == NULL )
+if (new == NULL)
 {
 return (NULL);
 }
+new->str = strdup(str);
 if (new->str == NULL)
 {
 free(new);
-return(NULL);
+return (NULL);
 }
 
 new->len = strlen(new->str);
