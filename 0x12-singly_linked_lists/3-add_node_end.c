e839ddb654c1a61a79e6068b9ce479b440765bab
diego <vandeldiegoc@gmail.com>
2020-03-20 22:56:46 -0500

    3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+/**
+ * _strlen - is a function
+ * @str: is a variable
+ *  Return: 0
+ */
+unsigned int _strlen(char *str)
+{
+unsigned int i;
+for (i = 0; str[i]; i++)
+{
+}
+return (i);
+}
+/**
+ * *add_node_end - is a function
+ * @head: is a variable
+ * @str: is a variable
+ *  Return: new
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+list_t *new, *ptr;
+if (str == NULL)
+{
+return (NULL);
+}
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->str = strdup(str);
+
+if (new->str == NULL)
+{
+free(new);
+return (NULL);
+}
+new->len = _strlen(new->str);
+new->next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+ptr = *head;
+while (ptr->next)
+{
+ptr = ptr->next;
+}
+ptr->next = new;
+return (new);
+}
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,12 +5,12 @@
 */
 void free_list(list_t *head)
 {
-list_t *ptr = head;
+list_t *ptrf;
 while (head != NULL)
 {
-free(head->str);
-free(head);
-ptr = ptrfree->next;
-head = ptr;
+ptrf = head;
+head = head->next;
+free(ptrf->str);
+free(ptrf);
 }
 }
