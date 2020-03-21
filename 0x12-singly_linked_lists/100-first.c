7e831ee21162938f75406e3eb195c5934ad05d8f
diego <vandeldiegoc@gmail.com>
2020-03-20 23:07:15 -0500

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void print(void)__attribute__((constructor));
+
+/**
+ * print- check the code for Holberton School students.
+ *
+ */
+
+void print(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
