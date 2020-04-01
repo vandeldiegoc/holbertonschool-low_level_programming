5d50a1c97988eb16e44d07c36d853340a5cbed04
Diego <vandeldiegoc@gmail.com>
2020-03-31 21:09:53 -0500

    1. Under the snow

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * create_file - create a file and write text in
+ * @filename: name of the file
+ * @text_content: limit of characters
+ * Return: 1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fb, c, num;
+fb = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fb == -1)
+		return (-1);
+
+	for (num = 0; text_content[num] != 0; num++)
+		;
+	c = write(fb, text_content, num);
+		if (c == -1)
+			return (-1);
+
+	close(fb);
+	return (1);
+}
