ff1bd7d0a66765cad8be99ec058e5bd302c2c23b
Diego <vandeldiegoc@gmail.com>
2020-05-02 17:16:36 -0500

    0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,10 +8,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 size_t i;
 while (h != NULL)
-	{
-	printf("%d\n", h->n);
-		h = h->next;
-		i++;
-	}
-	return (i);
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
 }
