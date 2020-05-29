fa3538c1d451d73dee0db4e4da9a60123c523554
Diego <vandeldiegoc@gmail.com>
2020-05-29 15:52:52 -0500

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,14 +8,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_array;
 
-	/* table creation */
 	hash_array = malloc(sizeof(hash_table_t));
 		if (hash_array == NULL)
 		{
 			return (NULL);
 		}
 
-	/* string pieces */
 	hash_array->array = calloc(size, sizeof(hash_node_t));
 	if (hash_array->array == NULL)
 	{
