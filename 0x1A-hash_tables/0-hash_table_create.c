bfc45e493581bf6fca3d495519d15562d56109d5
Diego <vandeldiegoc@gmail.com>
2020-05-29 15:57:34 -0500

    0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -4,23 +4,32 @@
  * @size: size of array
  * Return: Always EXIT_SUCCESS.
  */
+#include "hash_tables.h"
+/**
+ * hash_table_create - create a new hash table.
+ * @size: size of array
+ * Return: Always EXIT_SUCCESS.
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_array;
-
+	hash_node_t *node_array = NULL;
+	/* table creation */
 	hash_array = malloc(sizeof(hash_table_t));
 		if (hash_array == NULL)
 		{
 			return (NULL);
 		}
 
-	hash_array->array = calloc(size, sizeof(hash_node_t *));
-	if (hash_array->array == NULL)
+	/* string pieces */
+	node_array = malloc(sizeof(hash_node_t *) * size);
+	if (node_array == NULL)
 	{
 		free(hash_array);
 		return (NULL);
 	}
 
+	hash_array->array = &node_array;
 	hash_array->size = size;
 	return (hash_array);
 }
