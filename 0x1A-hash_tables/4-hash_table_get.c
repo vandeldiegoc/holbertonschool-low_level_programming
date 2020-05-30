1628067bf016e5b689fbfd726758a902ddfa5d6e
Diego <vandeldiegoc@gmail.com>
2020-05-29 23:02:05 -0500

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,10 +11,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 unsigned long int idx;
 hash_node_t *ptr;
-idx = key_index((unsigned char *)key, ht->size);
 if (!ht || !(ht->array) || !key || strlen(key) == 0)
 return (NULL);
 
+idx = key_index((unsigned char *)key, ht->size);
 ptr = ht->array[idx];
 while (ptr != NULL)
 {
