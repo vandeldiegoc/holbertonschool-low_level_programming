67b88af6b9e7eae018b75a30b0e0c40d1f13afd7
Diego <vandeldiegoc@gmail.com>
2020-05-29 19:47:39 -0500

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t  *node = NULL, *temp;
 
-	if (key == NULL || value == NULL || ht == NULL)
+	if (key == NULL || value == NULL || ht == NULL || ht->array == NULL)
 		return (0);
 
 	index = key_index((unsigned char *)key, ht->size);
