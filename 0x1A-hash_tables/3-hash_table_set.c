18efad1c65607c83789ecd5af333cc696896641b
Diego <vandeldiegoc@gmail.com>
2020-05-29 19:25:42 -0500

    0

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,17 +8,30 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t  *node = NULL;
+	hash_node_t  *node = NULL, *temp;
 
 	index = key_index((unsigned char *)key, ht->size);	
-	if (key == NULL || value == NULL)
+
+	temp = ht->array[index];
+	if (key == NULL || value == NULL || ht == NULL)
 	{
 		return 0;
+	}
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
+			else
+				return (1);
+		}
+		temp = temp->next;
 	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{	
-		printf("2");
 		return (0);
 	}
 	
@@ -27,8 +40,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->next = NULL;
 
 	if (ht->array[index] != NULL)
-		ht->array[index]->next = node;
-
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
 	else
 	{
 	       	ht->array[index] = node;
