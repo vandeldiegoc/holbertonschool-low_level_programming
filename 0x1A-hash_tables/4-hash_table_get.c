275040dc2be526483f3fd79cb90d3ede848968b7
Diego <vandeldiegoc@gmail.com>
2020-05-29 22:34:49 -0500

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - a function that retrieves a value associated with a key.
+ *
+ * @ht: hash table
+ * @key: a key you're looking for.
+ * Return: key.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+unsigned long int idx;
+idx = key_index((unsigned char *)key, ht->size);
+if (ht->array[idx] == NULL)
+return (NULL);
+
+while (ht->array[idx] != NULL)
+{
+if (strcmp(ht->array[idx]->key, key) == 0)
+{
+return (ht->array[idx]->value);
+}
+ht->array[idx] = ht->array[idx]->next;
+}
+return (NULL);
+}
