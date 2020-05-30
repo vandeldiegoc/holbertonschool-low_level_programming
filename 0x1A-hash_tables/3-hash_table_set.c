a1f8a9e60d2a6e818c24eea9139ffa7fc5715ea6
Diego <vandeldiegoc@gmail.com>
2020-05-29 20:04:09 -0500

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,21 +29,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
-	node = malloc(sizeof(hash_node_t *));
+
+	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 	{
 		return (0);
 	}
 	node->key = strdup(key);
 	node->value = strdup(value);
-	node->next = NULL;
-	if (ht->array[index] != NULL)
-	{
-		node->next = ht->array[index];
-		ht->array[index] = node;
-	}
-	else
-		ht->array[index] = node;
+	node->next = ht->array[index];
+	ht->array[index] = node;
 
 return (1);
 }
