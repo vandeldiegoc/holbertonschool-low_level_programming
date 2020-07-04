53ac58fd163bc2acd42a9a967edb9db68d51956d
Diego <vandeldiegoc@gmail.com>
2020-07-03 22:53:59 -0500

    5-island_perimeter.py[fix] pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """new method"""
+
+
 def island_perimeter(grid):
     """method get perimeter"""
     perimetro = 0
@@ -7,7 +9,7 @@ def island_perimeter(grid):
         for y in range(len(grid[x])):
             if grid[x][y] == 1 and grid[x - 1][y] == 0:
                 perimetro += 1
-            if grid[x][y] == 1 and grid[x][y -1] == 0:
+            if grid[x][y] == 1 and grid[x][y - 1] == 0:
                 perimetro += 1
             if grid[x][y] == 1 and grid[x][y + 1] == 0:
                 perimetro += 1
